749b088341efcfe7eae3091e6e1a23d98b22dd26
AnitaMohanta23 <124444765+AnitaMohanta23@users.noreply.github.com>
2023-02-06 22:26:14 +0530

    Add files via upload

---
+++ nested if.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int num=4;
+	if(num==1){
+		printf("one");
+	}
+	else if(num==2){
+		printf("two");
+	}
+	else if(num==3){
+		printf("three");
+	}
+	else if(num==4){
+		printf("four");
+	}
+	else{
+		printf("num is not in between 1 to 4");
+	}
+	return 0;
+}

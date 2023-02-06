5e4ebf6b8d1c5613171bd5a4f9701bdd845afdce
AnitaMohanta23 <124444765+AnitaMohanta23@users.noreply.github.com>
2023-02-06 21:46:44 +0530

    Add files via upload

---
+++ +Ve or -ve(WANR).cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void check();
+void check()
+{
+	int num=4;
+	if(num%2==0){
+		printf("number is positive");
+	}
+	else
+	{
+		printf("number is negative");
+	}
+}
+int main()
+{
+	check();
+	return 0;
+}

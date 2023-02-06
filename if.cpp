b53d5b060933821ff736354e5a65867bee0963a6
AnitaMohanta23 <124444765+AnitaMohanta23@users.noreply.github.com>
2023-02-06 22:21:22 +0530

    Add files via upload

---
+++ if.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a=1;
+	printf("Hello\n");
+	if(a>5){
+		printf("a is greater\n");
+	}
+	if(a==1){
+		printf("a is equal to 1\n");
+	}
+	printf("Hello\n");
+	return 0;
+	
+	
+}

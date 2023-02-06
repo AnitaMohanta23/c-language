0e28551f61aefcbc587c4a7373efbdfd79a762e3
AnitaMohanta23 <124444765+AnitaMohanta23@users.noreply.github.com>
2023-02-06 21:52:51 +0530

    Add files via upload

---
+++ control strc(if).cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int a=1;
+	printf("hello\n");
+	if(a>5){
+		printf("a is greater\n");
+		
+	}
+	if(a==1);
+	{
+		printf("a is equal to 1\n");
+		
+	}
+	printf("hello\n");
+	return 0;
+}

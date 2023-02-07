46efc276f9c2c328a59c8082a2a6cca9fda61819
AnitaMohanta23 <124444765+AnitaMohanta23@users.noreply.github.com>
2023-02-07 16:04:16 +0530

    Add files via upload

---
+++ pattern 12.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a,i;
+	for(a=1;a<=5;a++){
+		for(i=1;i<=5;i++){
+			if(a%2==0)
+			printf("%d",i);
+			else
+			printf(".");
+		}
+		printf(" \n");
+	}
+	return 0;
+}

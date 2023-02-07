c2d0c46f6ac1229064ebda9c4440fc308620e8cb
AnitaMohanta23 <124444765+AnitaMohanta23@users.noreply.github.com>
2023-02-07 15:58:49 +0530

    Add files via upload

---
+++ pattern 3.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int num=0;
+	num=60;
+	if(num==0)
+	{
+		printf("Number is zero");
+	}
+	else if(num>0)
+	{
+		printf("Number is positive");
+	}
+	else{
+		printf("Number is negative");
+	}
+	return 0;
+}
